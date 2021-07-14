0813fc6c9dbfd165ac3469294a497418f89a5ea1
morganfula <morgan.fula@me.com>
2021-07-14 21:12:47 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,94 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+import fragment from './shaders/fragment.glsl';
+import vertex from './shaders/vertex.glsl';
+
+export default class Sketch {
+  constructor(options) {
+    this.container = options.domElement;
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+
+    //! Camera
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      this.width / this.height,
+      0.01,
+      10
+    );
+    this.camera.position.z = 1;
+
+    //!Scene
+    this.scene = new THREE.Scene();
+
+    //!Renderer
+    this.renderer = new THREE.WebGLRenderer({
+      antialias: true,
+      alpha: true,
+    });
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    // this.renderer.setPixelRatio(2);
+    this.renderer.setSize(this.width, this.height);
+
+    this.container.appendChild(this.renderer.domElement);
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    document.addEventListener('keyup', this.applyWireframe);
+
+    this.time = 0;
+    this.resize();
+    this.addObjects();
+    this.render();
+
+    this.setupResize();
+  }
+
+  resize() {
+    this.width = this.container.offsetWidth;
+    this.height = this.container.offsetHeight;
+    //Update Renderer
+    this.renderer.setSize(this.width, this.height);
+    this.camera.aspect = this.width / this.height;
+    this.camera.updateProjectionMatrix();
+  }
+
+  setupResize() {
+    window.addEventListener('resize', this.resize.bind(this));
+  }
+
+  addObjects() {
+    this.geometry = new THREE.PlaneBufferGeometry(0.5, 0.5, 100, 100);
+    // this.material = new THREE.MeshNormalMaterial();
+
+    this.material = new THREE.ShaderMaterial({
+      wireframe: true,
+      uniforms: {
+        time: { value: 1.0 },
+        resolution: { value: new THREE.Vector2() },
+      },
+
+      vertexShader: vertex,
+      fragmentShader: fragment,
+    });
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material);
+    this.scene.add(this.mesh);
+  }
+
+  render() {
+    this.time += 0.05;
+    this.material.uniforms.time.value = this.time;
+    this.mesh.rotation.x = this.time / 2000;
+    this.mesh.rotation.y = this.time / 1000;
+
+    this.renderer.render(this.scene, this.camera);
+
+    requestAnimationFrame(this.render.bind(this));
+  }
+}
+
+new Sketch({
+  domElement: document.getElementById('container'),
+});
---
+++ shaders/vertex.glsl
@@ -0,0 +1,12 @@
+uniform float time;
+varying float pulse;
+
+varying vec2 vUv;
+
+void main() {
+  vUv = uv;
+  vec3 newPosition = position;
+  newPosition.z = 0.1* sin(length(position)* 30. + time);
+  pulse = 20.* newPosition.z;
+  gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+}
